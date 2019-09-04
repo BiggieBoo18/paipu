a74f3298e5e5e05a69efba890f039a38329a9a0a
BiggieBoo18 <ns182418@gmail.com>
2019-09-04 15:20:47 +0900

    temp commit

--- background/background.js
+++ background/background.js
@@ -1,26 +1,46 @@
-var targetPage      = "<all_urls>";
+var targetPage      = '<all_urls>';
 var connectedTabIds = {};
+
+function awaitOnMessage(p) {
+    return new Promise((resolve, reject) => {
+	p.onMessage.addListener(resolve);
+    });
+}
+
+async function asyncOnMessage(p) {
+    const m = await awaitOnMessage(p);
+    return m;
+}
 
 /**
  * Modify request headers
  */
-function modifyHeaders(e) {
-    if (connectedTabIds[e.tabId]===undefined) {
-	console.log("Please push a toolbar button");
-    } else {
+async function modifyHeaders(e) {
+    let edited_headers = undefined;
+    if (connectedTabIds[e.tabId]!==undefined) {
+	console.log(`start connection ${e.tabId}`);
 	let p = connectedTabIds[e.tabId];
 	p.postMessage({headers: e.requestHeaders});
+	edited_headers = await awaitOnMessage(p);
+	// let promise = asyncOnMessage(p);
+	// promise.then((m) => {
+	//     console.log("Recieve from edited headers");
+	//     console.dir(m.edited_headers);
+	//     console.log(`end connection ${e.tabId}`);
+	//     edited_headers = m.edited_headers
+	// });
     }
-    // e.requestHeaders.forEach(function(header){
-    // 	console.log(header);
-    // });
+    if (edited_headers===undefined) {
+	console.log("not wait");
+	console.dir(edited_headers);
+    }
     return {requestHeaders: e.requestHeaders};
 }
 
 browser.webRequest.onBeforeSendHeaders.addListener(
     modifyHeaders,
     {urls: [targetPage]},
-    ["blocking", "requestHeaders"]
+    ['blocking', 'requestHeaders']
 );
 
 /**
@@ -38,22 +58,22 @@ browser.browserAction.onClicked.addListener((tab) => {
 	let paipuURL = browser.extension.getURL(`popup/paipu.html`);
 	let creating = browser.windows.create({
 	    url: paipuURL,
-	    type: "popup",
+	    type: 'popup',
 	    height: 900,
 	    width: 800
 	});
 	creating.then(onCreated, onError);
 	browser.runtime.onConnect.addListener((p) => {
 	    connectedTabIds[tab.id] = p;
-	    console.log("background start greeting");
-	    p.postMessage({greeting:"Hi there content script"});
+	    console.log('background start greeting');
+	    p.postMessage({greeting:'Hi there content script'});
 	    p.onMessage.addListener((m) => {
-		console.log("From content script:"+m.greeting);
+		console.log('From content script:'+m.greeting);
 	    });
-	    console.log("background end greeting");
+	    console.log('background end greeting');
 	    console.dir(connectedTabIds);
 	});
     } else {
-	console.log("Already created a paipu window");
+	console.log('Already created a paipu window');
     }
 });
